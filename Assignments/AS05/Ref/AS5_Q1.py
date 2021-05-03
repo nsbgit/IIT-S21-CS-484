180096b731ca5729df485907c3dfa8bcc3069d59
Sukanta Sharma <sukantacse94@gmail.com>
2021-05-02 22:06:39 -0500

    q1 paraphrased

--- Assignments/AS05/Ref/AS5_Q1.py
+++ Assignments/AS05/Ref/AS5_Q1.py
@@ -27,71 +27,75 @@ TARGET_FEATURE = 'quality_grp'
 # -------------------------------
 
 # Read Data -------------------------------
-trainData = pandas.read_csv('WineQuality_Train.csv')
-testData = pandas.read_csv('WineQuality_Test.csv')
+train_Data = pandas.read_csv('WineQuality_Train.csv')
+test_Data = pandas.read_csv('WineQuality_Test.csv')
 # -------------------------------
-nObs = trainData.shape[0]
+nObs = train_Data.shape[0]
 
-x_train = trainData[INPUT_FEATURES]
-y_train = trainData[TARGET_FEATURE]
+x_train = train_Data[INPUT_FEATURES]
+y_train = train_Data[TARGET_FEATURE]
 
-x_test = trainData[INPUT_FEATURES]
-y_test = trainData[TARGET_FEATURE]
+x_test = train_Data[INPUT_FEATURES]
+y_test = train_Data[TARGET_FEATURE]
+
 # q1.a, b -------------------------------
 
 
 w_train = numpy.full(nObs, 1.0)
-accuracy = numpy.zeros(50)
-ensemblePredProb = numpy.zeros((nObs, 2))
+accuracy = numpy.zeros(MAX_BOOSTING_ITR)
+ensemble_predicted_probability = numpy.zeros((nObs, 2))
 is_converged = False
 coverged_accuracy = numpy.nan
 covereged_iteration = numpy.nan
 
-for iter in range(50):
-    classTree = tree.DecisionTreeClassifier(criterion=SPLITTING_CRITERION, max_depth=MAXIMUM_TREE_DEPTH, random_state=INIT_RNDM_SEED)
-    treeFit = classTree.fit(x_train, y_train, w_train)
-    treePredProb = classTree.predict_proba(x_train)
-    accuracy[iter] = classTree.score(x_train, y_train, w_train)
-    ensemblePredProb += accuracy[iter] * treePredProb
+for i in range(MAX_BOOSTING_ITR):
+    classification_tree = tree.DecisionTreeClassifier(criterion=SPLITTING_CRITERION, max_depth=MAXIMUM_TREE_DEPTH,
+                                                      random_state=INIT_RNDM_SEED)
+    tree_fit = classification_tree.fit(x_train, y_train, w_train)
+    tree_predicted_probability = classification_tree.predict_proba(x_train)
+    accuracy[i] = classification_tree.score(x_train, y_train, w_train)
+    ensemble_predicted_probability += accuracy[i] * tree_predicted_probability
 
-    if accuracy[iter] >= INTERRUPT_ACCURACY:
+    if accuracy[i] >= INTERRUPT_ACCURACY:
         is_converged = True
-        coverged_accuracy = accuracy[iter]
-        covereged_iteration = iter
+        coverged_accuracy = accuracy[i]
+        covereged_iteration = i
         break
-    
+
     # Update the weights
-    eventError = numpy.where(y_train == 1, (1 - treePredProb[:,1]), (treePredProb[:,1]))
-    predClass = numpy.where(treePredProb[:,1] >= 0.2, 1, 0)
-    w_train = numpy.where(predClass != y_train, 2+numpy.abs(eventError), numpy.abs(eventError))
+    event_error = numpy.where(
+        y_train == 1
+        , (1 - tree_predicted_probability[:, 1])
+        , (tree_predicted_probability[:, 1]))
+    predicted_class = numpy.where(tree_predicted_probability[:, 1] >= 0.2, 1, 0)
+    w_train = numpy.where(predicted_class != y_train, 2 + numpy.abs(event_error), numpy.abs(event_error))
 
-misclass=1-accuracy
+misclassification_rate = 1 - accuracy
 
-print(f'The Misclassification Rate of the classification tree on the Training data at Iteration  0 = {misclass[0]}')
-print(f'The Misclassification Rate of the classification tree on the Training data at Iteration  1 = {misclass[1]}')
-
+print(f'The Misclassification Rate of the classification tree on the Training data at Iteration  0 = {misclassification_rate[0]}')
+print(f'The Misclassification Rate of the classification tree on the Training data at Iteration  1 = {misclassification_rate[1]}')
 
 #  q1.c -------------------------------
 if is_converged:
-    print('The iteration is converged at Iteration {} and the Misclassification Rate of the classification tree on the Training data is {:.7f}'.format(covereged_iteration, 1 - coverged_accuracy))
+    print(
+        'The iteration is converged at Iteration {} and the Misclassification Rate of the classification tree on the Training data is {:.7f}'.format(
+            covereged_iteration, 1 - coverged_accuracy))
 else:
     print('The iteration is not converged')
-    
-    
+
 # q1.d -------------------------------
 
 
-y_score = treeFit.predict_proba(x_test)
-AUC = metrics.roc_auc_score(y_test,y_score[:,1])
-print(f'The Area Under Curve metric on the Testing data using the final converged classification tree is {AUC}')
-
+y_score = tree_fit.predict_proba(x_test)
+auc = metrics.roc_auc_score(y_test, y_score[:, 1])
+print(f'The Area Under Curve metric on the Testing data using the final converged classification tree is {auc}')
 
 # q1.e -------------------------------
 
 
-df3= pandas.concat(
-    [pandas.DataFrame(y_test), pandas.DataFrame(treePredProb[:,1])]
+df_box = pandas.concat(
+    [pandas.DataFrame(y_test), pandas.DataFrame(tree_predicted_probability[:, 1])]
     , axis=1
     , join='inner')
-df3 = df3.rename(columns={0:"Prediction"})
-df3.boxplot(column='Prediction', by='quality_grp')
+df_box = df_box.rename(columns={0: "Prediction"})
+df_box.boxplot(column='Prediction', by='quality_grp')
